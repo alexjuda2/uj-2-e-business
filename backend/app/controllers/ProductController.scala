3bbd91558462d7833723d79cb53e46195859ed96
alexjuda2 <alexanderjuda+2@gmail.com>
2021-06-22 22:47:25 +0200

    Add simple product controller

---
+++ backend/app/controllers/ProductController.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import com.google.inject.Singleton
+import models.SimpleProduct
+
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.libs.json.{Json, OFormat}
+
+import scala.collection.mutable
+
+@Singleton
+class ProductController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  /**
+   * Create an Action to render an HTML page with a welcome message.
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def index = Action {
+    val productList = new mutable.ListBuffer[SimpleProduct]()
+    productList.addOne(new SimpleProduct("foo"))
+
+    implicit val productListJson: OFormat[SimpleProduct] = Json.format[SimpleProduct]
+    
+    Ok(Json.toJson(productList))
+  }
+
+}
---
+++ backend/app/models/SimpleProduct.scala
@@ -0,0 +1,3 @@
+package models
+
+case class SimpleProduct(name: String)
