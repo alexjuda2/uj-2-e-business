df5afe422a171cdeb0f1f0ddd3766dd565bee507
alexjuda2 <alexanderjuda+2@gmail.com>
2021-07-31 16:22:22 +0200

    Fix google auth redirects

--- backend/app/controllers/SocialAuthController.scala
+++ backend/app/controllers/SocialAuthController.scala
@@ -22,7 +22,7 @@ class SocialAuthController @Inject()(scc: DefaultSilhouetteControllerComponents,
             _ <- authInfoRepository.save(profile.loginInfo, authInfo)
             authenticator <- authenticatorService.create(profile.loginInfo)
             value <- authenticatorService.init(authenticator)
-            result <- authenticatorService.embed(value, Redirect("http://localhost:3000"))
+            result <- authenticatorService.embed(value, Redirect("http://localhost:9000"))
           } yield {
             val Token(name, value) = CSRF.getToken.get
             result.withCookies(Cookie(name, value, httpOnly = false))
@@ -30,8 +30,8 @@ class SocialAuthController @Inject()(scc: DefaultSilhouetteControllerComponents,
         }
       case _ => Future.failed(new ProviderException(s"Cannot authenticate with unexpected social provider $provider"))
     }).recover {
-      case _: ProviderException =>
-        Forbidden("Forbidden")
+      case e: ProviderException =>
+        Forbidden("Forbidden" + e.getMessage())
     }
   })
 }
