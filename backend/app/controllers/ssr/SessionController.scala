06640a4a2f2755d7b23b80fbdab5fce12a0bae3f
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-01 22:54:24 +0200

    Remove session cookies on sign out

--- backend/app/controllers/ssr/SessionController.scala
+++ backend/app/controllers/ssr/SessionController.scala
@@ -1,23 +1,19 @@
 package controllers.ssr
 
-import com.mohiva.play.silhouette.api.actions.SecuredRequest
 import controllers.{AbstractAuthController, DefaultSilhouetteControllerComponents}
-import play.api.mvc.{Action, AnyContent, DiscardingCookie}
-import play.filters.csrf.CSRFAddToken
+import play.api.mvc.DiscardingCookie
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class SessionController @Inject()(scc: DefaultSilhouetteControllerComponents, addToken: CSRFAddToken)(implicit ex: ExecutionContext) extends AbstractAuthController(scc) {
-  def signOut = silhouette.SecuredAction { implicit request =>
-//  def signOut: Action[AnyContent] = securedAction { implicit request: SecuredRequest[EnvType, AnyContent] =>
-    Ok("yasss")
-//    authenticatorService.discard(request.authenticator, Ok("Signed out"))
-//      .map(_.discardingCookies(
-//        DiscardingCookie(name = "csrfToken"),
-//        DiscardingCookie(name = "PLAY_SESSION"),
-//        DiscardingCookie(name = "OAuth2State")
-//      ))
+class SessionController @Inject()(scc: DefaultSilhouetteControllerComponents)(implicit ex: ExecutionContext) extends AbstractAuthController(scc) {
+  def signOut = silhouette.SecuredAction.async { implicit request =>
+    authenticatorService.discard(request.authenticator, Ok("Signed out"))
+      .map(_.discardingCookies(
+        DiscardingCookie(name = "csrfToken"),
+        DiscardingCookie(name = "PLAY_SESSION"),
+        DiscardingCookie(name = "OAuth2State")
+      ))
   }
 }
