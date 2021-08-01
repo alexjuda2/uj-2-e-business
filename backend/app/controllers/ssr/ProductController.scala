50a67e2644e0aabe61b76e1a8d87b5e9ac3296e0
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-01 23:02:21 +0200

    List all products with, two formats with single controller

---
+++ backend/app/controllers/ssr/ProductController.scala
@@ -0,0 +1,27 @@
+package controllers.ssr
+
+import controllers.{AbstractAuthController, DefaultSilhouetteControllerComponents}
+
+import javax.inject.{Inject, Singleton}
+import models.ProductRepo
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent}
+
+import scala.concurrent.ExecutionContext
+
+
+@Singleton
+class ProductController @Inject()(productRepo: ProductRepo, scc: DefaultSilhouetteControllerComponents)(implicit ex: ExecutionContext) extends AbstractAuthController(scc) {
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def getProducts: Action[AnyContent] = Action.async { implicit request =>
+    val productsFuture = productRepo.all()
+    productsFuture.map(products => render {
+      case Accepts.Html() => Ok(views.html.products(products))
+      case Accepts.Json() => Ok(Json.toJson(products))
+    })
+  }
+
+}
