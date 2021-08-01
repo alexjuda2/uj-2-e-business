a324af1c2d1d7b744c99281c0cca6b521ac586c9
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-01 21:47:47 +0200

    Show logged user email

--- backend/app/controllers/ssr/HomeController.scala
+++ backend/app/controllers/ssr/HomeController.scala
@@ -1,14 +1,19 @@
 package controllers.ssr
 
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import controllers.{AbstractAuthController, DefaultSilhouetteControllerComponents}
+import play.api.mvc.{Action, AnyContent}
 
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
 
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(scc: DefaultSilhouetteControllerComponents)(implicit ex: ExecutionContext) extends AbstractAuthController(scc) {
 
-  def index: Action[AnyContent] = Action {
-    Ok(views.html.ssr.home.index())
+  def index = silhouette.UserAwareAction { implicit request =>
+    request.identity match {
+      case Some(identity) => Ok(views.html.ssr.home.index(identity.email))
+      case None => Ok(views.html.ssr.home.index("Noone, really"))
+    }
   }
 
 }
