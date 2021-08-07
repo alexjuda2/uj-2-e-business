5b3e7a3aa1094adff1cc82caf2876448b90e138f
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-07 14:02:23 +0200

    Remove usage of hardcoded url

--- backend/app/controllers/SocialAuthController.scala
+++ backend/app/controllers/SocialAuthController.scala
@@ -22,7 +22,7 @@ class SocialAuthController @Inject()(scc: DefaultSilhouetteControllerComponents,
             _ <- authInfoRepository.save(profile.loginInfo, authInfo)
             authenticator <- authenticatorService.create(profile.loginInfo)
             value <- authenticatorService.init(authenticator)
-            result <- authenticatorService.embed(value, Redirect("http://localhost:9000"))
+            result <- authenticatorService.embed(value, Redirect(controllers.ssr.routes.HomeController.index))
           } yield {
             val Token(name, value) = CSRF.getToken.get
             result.withCookies(Cookie(name, value, httpOnly = false))
