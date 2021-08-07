193a0ebfc8a39606980138fdc24a4bbc4c795674
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-07 23:49:27 +0200

    Use rails convention for naming actions

--- backend/app/controllers/ssr/CategoryController.scala
+++ backend/app/controllers/ssr/CategoryController.scala
@@ -12,11 +12,15 @@ import scala.concurrent.ExecutionContext
 
 @Singleton
 class CategoryController @Inject()(categoryRepo: CategoryRepo, scc: DefaultSilhouetteControllerComponents)(implicit ex: ExecutionContext) extends AbstractAuthController(scc) {
-  def getCategories: Action[AnyContent] = Action.async { implicit request =>
+  def getAll: Action[AnyContent] = Action.async { implicit request =>
     categoryRepo.all().map(categories => render {
       case Accepts.Html() => Ok(views.html.ssr.categories.index(categories))
       case Accepts.Json() => Ok(Json.toJson(categories))
     })
   }
 
+  def _new: Action[AnyContent] = Action { implicit request =>
+    // This only makes sense for SSR page
+    Ok(views.html.ssr.categories.edit(Option.empty))
+  }
 }
