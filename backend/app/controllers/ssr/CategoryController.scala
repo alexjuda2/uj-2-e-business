a95dd2d43d8befafd120eb7931faf9b0d7e06fb5
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-08 00:22:45 +0200

    Add new/create category actions

--- backend/app/controllers/ssr/CategoryController.scala
+++ backend/app/controllers/ssr/CategoryController.scala
@@ -3,16 +3,26 @@ package controllers.ssr
 import controllers.{AbstractAuthController, DefaultSilhouetteControllerComponents}
 
 import javax.inject.{Inject, Singleton}
-import models.{CategoryRepo, ProductRepo}
+import models.CategoryRepo
+import play.api.data.Form
+import play.api.data.Forms.{mapping, nonEmptyText}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent}
+import play.filters.csrf.CSRF
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
+case class CreateCategoryForm(name: String)
 
 @Singleton
 class CategoryController @Inject()(categoryRepo: CategoryRepo, scc: DefaultSilhouetteControllerComponents)(implicit ex: ExecutionContext) extends AbstractAuthController(scc) {
-  def getAll: Action[AnyContent] = Action.async { implicit request =>
+  val createCategoryForm: Form[CreateCategoryForm] = Form {
+    mapping(
+      "name" -> nonEmptyText
+    )(CreateCategoryForm.apply)(CreateCategoryForm.unapply)
+  }
+
+  def all: Action[AnyContent] = Action.async { implicit request =>
     categoryRepo.all().map(categories => render {
       case Accepts.Html() => Ok(views.html.ssr.categories.index(categories))
       case Accepts.Json() => Ok(Json.toJson(categories))
@@ -21,6 +31,25 @@ class CategoryController @Inject()(categoryRepo: CategoryRepo, scc: DefaultSilho
 
   def _new: Action[AnyContent] = Action { implicit request =>
     // This only makes sense for SSR page
-    Ok(views.html.ssr.categories.edit(Option.empty))
+
+    val csrfToken = CSRF.getToken.get
+    Ok(views.html.ssr.categories._new(csrfToken))
   }
+
+  def create: Action[AnyContent] = Action.async { implicit request =>
+    val csrfToken = CSRF.getToken.get
+    createCategoryForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest(views.html.ssr.categories._new(csrfToken))
+        )
+      },
+      category => {
+        categoryRepo.create(category.name).map { _ =>
+          Redirect(controllers.ssr.routes.CategoryController.all)
+        }
+      }
+    )
+  }
+
 }
