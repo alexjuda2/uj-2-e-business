6462953b0280341d1d54438bb5b584d9115e3a0d
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-08 20:37:17 +0200

    Add wishList & wishListItem tables

--- backend/conf/evolutions/default/3.sql
+++ backend/conf/evolutions/default/3.sql
@@ -21,7 +21,28 @@ CREATE TABLE "cartItem"
     FOREIGN KEY (user) references user (id)
 );
 
+CREATE TABLE "wishList"
+(
+    "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" VARCHAR NOT NULL,
+    "user"     INTEGER NOT NULL,
+
+    FOREIGN KEY (user) references user (id)
+);
+
+CREATE TABLE "wishListItem"
+(
+    "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "product"  INTEGER NOT NULL,
+    "wishList" INTEGER NOT NULL,
+
+    FOREIGN KEY (product) references product (id),
+    FOREIGN KEY (wishList) references wishList (id)
+);
+
 --- !Downs
 
 DROP TABLE "review";
 DROP TABLE "cartItem";
+DROP TABLE "wishList";
+DROP TABLE "wishListItem";
