b060945eb7088ff2cb36374d4a3fdc23b13bfb06
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-08 21:09:34 +0200

    Add tables for order & orderItem

--- backend/conf/evolutions/default/3.sql
+++ backend/conf/evolutions/default/3.sql
@@ -40,9 +40,31 @@ CREATE TABLE "wishListItem"
     FOREIGN KEY (wishList) references wishList (id)
 );
 
+CREATE TABLE "order"
+(
+    "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "address"  VARCHAR NOT NULL,
+    "user" INTEGER NOT NULL,
+
+    FOREIGN KEY (user) references user (id)
+);
+
+CREATE TABLE "orderItem"
+(
+    "id"       INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "quantity" INTEGER NOT NULL,
+    "product"  INTEGER NOT NULL,
+    "order" INTEGER NOT NULL,
+
+    FOREIGN KEY (product) references product (id),
+    FOREIGN KEY (`order`) references `order` (id)
+);
+
 --- !Downs
 
 DROP TABLE "review";
 DROP TABLE "cartItem";
 DROP TABLE "wishList";
 DROP TABLE "wishListItem";
+DROP TABLE "order";
+DROP TABLE "orderItem";
