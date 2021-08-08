c5b3e8517ae83e7beb21cb0bbc882937c7666e09
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-09 00:05:35 +0200

    Add frontent app text to navbar

--- frontend/src/comp/Navbar.js
+++ frontend/src/comp/Navbar.js
@@ -3,16 +3,19 @@ import React from "react";
 export default function Navbar({ sections }) {
     return (
         <nav>
-            <div className="nav-wrapper">
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    {sections.map((section, sectionI) => {
-                        return (
-                            <li key={sectionI}>
-                                <a onClick={section.onClick}>{section.text}</a>
-                            </li>
-                        );
-                    })}
-                </ul>
+            <div className="container">
+                <a href="#" className="brand-logo">frontend app</a>
+                <div className="nav-wrapper">
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        {sections.map((section, sectionI) => {
+                            return (
+                                <li key={sectionI}>
+                                    <a onClick={section.onClick}>{section.text}</a>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
             </div>
         </nav>
     );
