db68eab23c79c4e2d7b8fdd2dca250cae2338a0e
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-08 19:16:00 +0200

    Add script for generating resources

---
+++ scripts/gen_resource.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# TARGET="backend/app/models/PromotionRepo.scala"
+# TARGET="backend/app/controllers/ssr/PromotionController.scala"
+# TARGET="backend/app/views/ssr/promotions/_new.scala.html"
+TARGET="backend/app/views/ssr/promotions/index.scala.html"
+
+sed -i 's/currency/promotion/g' $TARGET
+sed -i 's/currencies/promotions/g' $TARGET
+sed -i 's/Currency/Promotion/g' $TARGET
+sed -i 's/Currencies/Promotions/g' $TARGET
