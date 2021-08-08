45f6340077328f4197f551d9db7fd65e8d75606e
alexjuda2 <alexanderjuda+2@gmail.com>
2021-08-08 19:26:05 +0200

    Add looping

--- scripts/rename_resource.sh
+++ scripts/rename_resource.sh
@@ -3,12 +3,19 @@
 set -e
 set -x
 
-# TARGET="backend/app/models/PromotionRepo.scala"
-# TARGET="backend/app/controllers/ssr/PromotionController.scala"
-# TARGET="backend/app/views/ssr/promotions/_new.scala.html"
-# TARGET="backend/app/views/ssr/promotions/index.scala.html"
+declare -a TARGETS=(
+    "backend/app/models/ProductPromotion.scala"
+    "backend/app/controllers/ssr/ProductPromotionController.scala"
+    "backend/app/views/ssr/productPromotions/_new.scala.html"
+    "backend/app/views/ssr/productPromotions/edit.scala.html"
+    "backend/app/views/ssr/productPromotions/index.scala.html"
+)
 
-sed -i 's/currency/promotion/g' $TARGET
-sed -i 's/currencies/promotions/g' $TARGET
-sed -i 's/Currency/Promotion/g' $TARGET
-sed -i 's/Currencies/Promotions/g' $TARGET
+
+for TARGET in "${TARGETS[@]}"
+do
+    sed -i 's/currency/promotion/g' $TARGET
+    sed -i 's/currencies/promotions/g' $TARGET
+    sed -i 's/Currency/Promotion/g' $TARGET
+    sed -i 's/Currencies/Promotions/g' $TARGET
+done
